4c39f2c0be73c8c32ba02351b471b9afe82da6de
KaiserPhemi <kaiser.phemi@protonmail.ch>
2020-05-07 00:21:22 +0100

    ft(chore)
    - refactor

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,4 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
+// react libraries
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
@@ -25,7 +19,6 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata.title} />
       <div
         style={{
           margin: `0 auto`,
